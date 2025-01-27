73766ab575693160ef62ac8c9764d7097ab6ef1a
Julie <juliet@ksu.edu>
2025-01-27 13:23:08 -0600

    last minute edit

--- week2_jan27-31/Encapsulation/Encapsulation/Program.cs
+++ week2_jan27-31/Encapsulation/Encapsulation/Program.cs
@@ -3,12 +3,11 @@
 public class Program
 {
     private static List<string> _names;
-    private static Dictionary<string, List<int>> _scores;
-    private static Dictionary<string, string> _majors;
+    private static List<List<int>> _scores;
+    private static List<string> _majors;
 
     public static void Main(string[] args)
     {
-        /*
         _names = new();
         _scores = new();
         _majors = new();
@@ -21,8 +20,6 @@ public class Program
         //write get average method
 
         //change major of Bob to MATH
-
-        */
     }
 
     public static int GetAverage(string name)
