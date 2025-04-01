a66e189449aa9ca70f0d5a5cd75fc2c7da5f9b42
Julie <juliet@ksu.edu>
2025-04-01 14:15:05 -0500

    before wed april 2

---
+++ week10_march31-april4/ValidationExample/ValidationExample/CalcViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationExample
+{
+    public class CalcViewModel
+    {
+
+        private uint _shirts = 0;
+        public uint Shirts
+        {
+            get => _shirts;
+            set
+            {
+                _shirts = value;
+
+                //throw exception if #shirts is invalid (more than 10)
+
+                //how to make sure cost is updated on GUI?
+            }
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                //$10 each for up to 5 shirts, $8 each after that
+
+                return 0.00m;
+            }
+        }
+    }
+}
